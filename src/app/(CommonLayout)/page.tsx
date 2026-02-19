ed4e8616385f6fb3a0b0e6da027e460a52241226
Md Meherab Hossain Munna <mdmeherab657@gamil.com>
2026-02-19 15:40:48 +0600

    style:TutorSection

--- src/app/(CommonLayout)/page.tsx
+++ src/app/(CommonLayout)/page.tsx
@@ -1,14 +1,14 @@
 import HeroSection from '@/components/layout/Hero';
 import CategorySection from '@/components/layout/Services';
 import AboutSection from '@/components/layout/Session';
-import TutoreHomeLayout from '@/components/layout/TutoreHomeLayout';
 import ServicesSection from '@/components/layout/ServicesSection';
+import TutorSection from '@/components/layout/TutorSection';
 
 export default async function Home() {
   return (
     <div className="">
       <HeroSection></HeroSection>
-      <TutoreHomeLayout></TutoreHomeLayout>
+      <TutorSection></TutorSection>
       <ServicesSection></ServicesSection>
       <CategorySection></CategorySection>
       <AboutSection></AboutSection>
